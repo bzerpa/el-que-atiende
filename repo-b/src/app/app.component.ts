e590b2748b4426250ac566bf4dc5d13185da0ecb
bzerpa <brunozerpa@gmail.com>
2025-07-11 15:53:22 -0300

    Update app.component.ts

--- repo-b/src/app/app.component.ts
+++ repo-b/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Cosa del fobal ';
+  title = 'Cosa del fobal locoooo';
 }
