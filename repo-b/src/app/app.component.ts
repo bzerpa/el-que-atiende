350f756a08f6e0a570f8d2392658cb38a13fd225
bzerpa <brunozerpa@gmail.com>
2025-07-15 11:41:40 -0300

    Update app.component.ts

--- repo-b/src/app/app.component.ts
+++ repo-b/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Ganador del ';
+  title = 'Ganador de la casa rosada';
 }
